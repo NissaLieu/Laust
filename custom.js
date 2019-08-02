13439d1566e419d37894f70b63b1c57020a47afd
NissaLieu <35055074+NissaLieu@users.noreply.github.com>
2019-08-02 09:23:17 -0700

    Delete custom.js

--- custom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready( function(){
-        $('input[type="file"]').change( function(){
-                $(this).next()..removeAttr('disabled');
-        } ).next().attr('disabled','disabled');
-});
-
